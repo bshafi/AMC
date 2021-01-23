84453e3da665b0e7b00d2df722c7f98eefff3598
Bilal Shafi <bshaf001@ucr.edu>
2021-01-23 14:06:14 -0800

    Made includes consistent with header file names

--- src/standard.hpp
+++ src/standard.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
-#include <GL/glew.h>
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL_ttf/SDL_ttf.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "gl_helper.hpp"
-
-// TODO: Rename this file sdl_gl_headers.hpp
-// TODO: Remove non opengl and non sdl header files from the include list
+#include <cassert>
+#include <cstdint>
+#include <string>
