cb8916cfd1cf1b589942a85be26266642ff750ca
Bilal Shafi <bshaf001@ucr.edu>
2021-01-27 16:14:11 -0800

    cleaned up header files

--- src/standard.hpp
+++ src/standard.hpp
@@ -7,3 +7,10 @@
 #include <cassert>
 #include <cstdint>
 #include <string>
+#include <vector>
+#include <variant>
+#include <map>
+#include <optional>
+
+template <typename A, typename B>
+using pair_vector = std::vector<std::pair<A, B>>;
